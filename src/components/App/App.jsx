7a1d51fef660373db7b9c526a91653f5cac65092
Padalka Sergey <serega443@gmail.com>
2023-01-05 15:11:22 +0200

    add localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -57,6 +57,21 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const filterContacts = this.onFilterContacts();
